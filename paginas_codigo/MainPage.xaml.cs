08e997215883d3de5146bb2e0499735b85404469
EndGabs <165812392+EndGabs@users.noreply.github.com>
2024-04-02 12:36:41 -0300

    Add files via upload

---
+++ paginas_codigo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRolar_Clicked(object sender, EventArgs e)
+        {
+            int opcao = Convert.ToInt32(PickerDado.SelectedItem)+1;
+            Random random = new();
+            NumeroSortido.Text = Convert.ToString(random.Next(1, opcao));
+        }
+    }
+
+}
